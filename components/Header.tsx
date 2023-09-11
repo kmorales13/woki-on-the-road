c8a1ac877fb9b9865b0041a4d6454cc8a9d4627c
kmorales13 <equinoxstudiosteam@gmail.com>
2023-09-10 19:37:16 -0500

    init

---
+++ components/Header.tsx
@@ -0,0 +1,53 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.jpg'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import SearchButton from './SearchButton'
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-10">
+      <div>
+        <Link href="/" aria-label={siteMetadata.headerTitle}>
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+              <Image
+                style={{ borderRadius: 8 }}
+                height={50}
+                width={50}
+                src={Logo.src}
+                alt="Woki On The Road Logo"
+              />
+            </div>
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <div className="hidden h-6 text-2xl font-semibold sm:block">
+                {siteMetadata.headerTitle}
+              </div>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
+        {headerNavLinks
+          .filter((link) => link.href !== '/')
+          .map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
+            >
+              {link.title}
+            </Link>
+          ))}
+        <SearchButton />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
+
+export default Header
