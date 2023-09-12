84193119f17fbc9c8167c2049b604631b907e9ee
kmorales13 <equinoxstudiosteam@gmail.com>
2023-09-12 02:34:42 -0500

    speed improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <Link href="/" aria-labelledby="headerTitle">
           <div className="flex items-center justify-between">
             <div className="mr-3">
               <Image
@@ -22,7 +22,7 @@ const Header = () => {
               />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div id="headerTitle" className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
